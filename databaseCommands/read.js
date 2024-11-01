6280e95b5744c6b589947649a16240c9e3ca3c17
Cole <coltonflather@gmail.com>
2024-10-31 20:58:54 -0400

    added emailing

--- databaseCommands/read.js
+++
@@ -1,1 +0,0 @@
-const {usersModel} = require('./models');
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const {usersModel} = require('./databaseCommands/models');
+const {usersModel} = require('./models');
+//const oAuth2Client = require("./getEmailToken")
 require("dotenv").config();
 
 const app = express();
@@ -25,6 +26,38 @@ app.get('/api/getUser/:username',(req,res)=>{
       res.status(200);res.send(result)
     }).catch(err=>res.send(err));
 })
+app.post('/api/addActivityForm', (req,res)=>{
+    const username = req.body["userame"]
+    const activity = req.body["activity"]
+
+})
+app.post('/api/addActivityPdf', (req,res)=>{
+
+})
+app.post('/api/addUser', (req,res)=>{
+
+})
+
+
+
+
+// app.get('/oauth2callback', async (req, res) => {
+//     const code = req.query.code;
+  
+//     if (code) {
+//       try {
+//         const { tokens } = await oAuth2Client.getToken(code);
+//         oAuth2Client.setCredentials(tokens);
+//         res.send('Authorization successful! You can close this tab.');
+//         console.log('Tokens received:', tokens);
+//       } catch (err) {
+//         console.error('Error exchanging code for tokens:', err.response?.data || err.message);
+//         res.send(`Error during authorization: ${err.message}`);
+//       }
+//     } else {
+//       res.send('No code provided');
+//     }
+//   });
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
